e91ccffb2d492b6914eef71c66f39ccf5d702f9e
SparklyGem <SparklyGem@users.noreply.github.com>
2015-08-25 17:50:31 -0400

    Added appendToId argument to button() function; updated comments

--- EasyHTML.js
+++ EasyHTML.js
@@ -1,17 +1,23 @@
 var easyHTML = {
-  /* Makes a button with a value (text, an image, or any HTML element), what to do when it's clicked, and, optionally,
-  give it an id */
-  button: function(value, event, id) {
+  /* Makes a button with a value (text, an image, or any HTML element), tells it what to do when it's clicked,
+  gives it an id, and, optionally, tells it which id the button should be appended to. If left blank, button will
+  be appended to the body.*/
+  button: function(value, event, id, appendToId) {
     var btn = document.createElement("button");
     btn.innerHTML = value;
     btn.addEventListener("click", event);
     btn.id = id;
-    document.body.appendChild(btn);
+    if (appendToId) {
+      var appendId = document.getElementById(appendToId);
+      appendId.appendChild(btn);
+    } else {
+      document.body.appendChild(btn);
+    }  
   },
-  /* Makes a table. The first argument, topLabels, takes an array of values (any HTML elements, but usually just text) that
-  will make up the labels at the top of the table. The second argument, rowData, takes an array of arrays, each array
-  containing the data that will make up one row of the table. The least argument is optional, and is the id to give the
-  table.*/
+  /* Makes a table. The first argument, topLabels, takes an array of values (any HTML elements, but usually just
+  text) that will make up the labels at the top of the table. The second argument, rowData, takes an array of
+  arrays, each array containing the data that will make up one row of the table. The last argument is optional,
+  and is the id to give the table.*/
   table: function(topLabels, rowData, id) {
     var tbl = "<table id=" + id + "><thead><tr>";
     for (var i = 0; i < topLabels.length; i++) {
